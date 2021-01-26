8bb8ff8a84f0afde392580d644b46a23f71c79e8
Jesper <jeskri97@gmail.com>
2021-01-26 17:51:09 +0100

    Added Task 1 and Task 2

---
+++ Lab5/DrawingShapes/DrawingShapes/shapes.cpp
@@ -0,0 +1,113 @@
+
+#include <iostream>
+#include <math.h>
+
+#include "shapes.h"
+
+Shape::Shape(Point2D pos, int color[4]) {
+	this->pos = pos;
+	for (int i = 0; i < 4; i++) {
+		if (color[i] < 0 || color[i] > 255) {
+			printf("\nERROR: Invalid color value, setting color value of ");
+			switch (i)
+			{
+			case 0:
+				printf("Red to 0\n");
+				this->color[i] = 0;
+				break;
+			case 1:
+				printf("Green to 0\n");
+				this->color[i] = 0;
+				break;
+			case 2:
+				printf("Blue to 0\n");
+				this->color[i] = 0;
+				break;
+			case 3:
+				printf("Alpha to 0\n");
+				this->color[i] = 0;
+				break;
+			default:
+				break;
+			}
+		}
+		else
+			this->color[i] = color[i];
+	}
+}
+Point2D Shape::getPos() {
+	return this->pos;
+}
+int* Shape::getColor() {
+	return this->color;
+}
+
+Rectangle::Rectangle(Point2D pos, int color[4], float width, float height)
+	: Shape(pos, color) {
+	this->width = width;
+	this->height = height;
+	this->halfWidth = width / 2;
+	this->halfHeight = height / 2;
+}
+void Rectangle::render(SDL_Renderer* renderer) {
+	Point2D pos = this->getPos();
+	std::string sPos = pos.toString();
+	int* color = this->getColor();
+	printf("\nRectangle\nPos:\t%s\nWidth / Height:\t%g / %g\n", sPos.c_str(), this->width, this->height);
+
+	// Set draw color
+	SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], color[3]);
+	// Draw shape
+	SDL_RenderDrawLine(renderer, pos.getx() + halfWidth, pos.getx() - halfHeight, pos.getx() - halfWidth, pos.getx() - halfHeight);
+	SDL_RenderDrawLine(renderer, pos.getx() - halfWidth, pos.getx() - halfHeight, pos.getx() - halfWidth, pos.getx() + halfHeight);
+	SDL_RenderDrawLine(renderer, pos.getx() - halfWidth, pos.getx() + halfHeight, pos.getx() + halfWidth, pos.getx() + halfHeight);
+	SDL_RenderDrawLine(renderer, pos.getx() + halfWidth, pos.getx() + halfHeight, pos.getx() + halfWidth, pos.getx() - halfHeight);
+}
+
+Triangle::Triangle(Point2D pos, int color[4], float base, float height)
+	: Shape(pos, color) {
+	this->base = base;
+	this->height = height;
+	this->halfBase = base / 2;
+	this->halfHeight = height / 2;
+}
+void Triangle::render(SDL_Renderer* renderer) {
+	Point2D pos = this->getPos();
+	std::string sPos = pos.toString();
+	int* color = this->getColor();
+	printf("\nTriangle\nPos:\t%s\nBase / Height:\t%g / %g\n", sPos.c_str(), this->base, this->height);
+
+	// Set draw color
+	SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], color[3]);
+	// Draw shape
+	SDL_RenderDrawLine(renderer, pos.getx(), pos.getx() - halfHeight, pos.getx() - halfBase, pos.getx() + halfHeight);
+	SDL_RenderDrawLine(renderer, pos.getx() - halfBase, pos.getx() + halfHeight, pos.getx() + halfBase, pos.getx() + halfHeight);
+	SDL_RenderDrawLine(renderer, pos.getx() + halfBase, pos.getx() + halfHeight, pos.getx(), pos.getx() - halfHeight);
+}
+
+Circle::Circle(Point2D pos, int color[4], float radius)
+	: Shape(pos, color) {
+	this->radius = radius;
+}
+void Circle::render(SDL_Renderer* renderer) {
+	Point2D pos = this->getPos();
+	std::string sPos = pos.toString();
+	int* color = this->getColor();
+	printf("\nTriangle\nPos:\t%s\nRadius:\t%g\n", sPos.c_str(), this->radius);
+
+	// Set draw color
+	SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], color[3]);
+	// Draw shape
+	int step = 20;
+	int degree = 0;
+	while (degree < 360) {
+		// i = degrees
+		float x1 = cos(degree * PI / 180) * this->radius * pos.getx();
+		float y1 = sin(degree * PI / 180) * this->radius * pos.gety();
+		degree += step;
+		float x2 = cos(degree * PI / 180) * this->radius * pos.getx();
+		float y2 = sin(degree * PI / 180) * this->radius * pos.gety();
+
+		SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+	}
+}
---
+++ Lab5/DrawingShapes/DrawingShapes/shapes.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "SDL.h"
+#include <iostream>
+#include "point2D.h"
+
+#ifndef PI
+#define PI    3.14159265358979323846264338327950288
+#endif
+
+class Shape {
+private:
+	// The shapes position
+	Point2D pos;
+	// Red, Green, Blue, Alpha
+	int color[4];
+public:
+	Shape(Point2D pos, int color[4]);
+
+	virtual void render(SDL_Renderer* renderer) = 0;
+
+	Point2D getPos();
+	int* getColor();
+};
+
+class Rectangle : public Shape {
+private:
+	float width, height, halfWidth, halfHeight;
+public:
+	Rectangle(Point2D pos, int color[4], float width, float height);
+	void render(SDL_Renderer* renderer);
+};
+
+class Triangle : public Shape {
+private:
+	float base, height, halfBase, halfHeight;
+public:
+	Triangle(Point2D pos, int color[4], float base, float height);
+	void render(SDL_Renderer* renderer);
+};
+
+class Circle : public Shape {
+private:
+	float radius;
+public:
+	Circle(Point2D pos, int color[4], float radius);
+	void render(SDL_Renderer* renderer);
+};
